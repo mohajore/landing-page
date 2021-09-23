5f22a63b62f97350f5a24efdc0e057a689b7fa76
mohammad aldokh <mohammad.aldokh@merwas.net>
2021-09-23 17:00:24 +0300

    (9/23/2021) the 2nd edit

--- src/components/Pages/home/CheckOut.jsx
+++ src/components/Pages/home/CheckOut.jsx
@@ -6,11 +6,33 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.scss"; // Swiper Carousel Style
 import SwiperCore from "swiper";
 import Title from "../../blocks/Title";
-//
-
-//
+// React elastic carousel start
+import Carousel, { consts } from "react-elastic-carousel";
+// React elastic carousel end
 
 class CheckOut extends Component {
+  myArrow({ type, onClick, isEdge }) {
+    const pointer =
+      type === consts.PREV ? (
+        <i class="fas fa-long-arrow-alt-left fa-2x "></i>
+      ) : (
+        <i class="fas fa-long-arrow-alt-right fa-2x "></i>
+      );
+    return (
+      <button onClick={onClick} disabled={isEdge}>
+        {pointer}
+      </button>
+    );
+  }
+  constructor(props) {
+    super(props);
+    this.breakPoints = [
+      { width: 1, itemsToShow: 1 },
+      { width: 549, itemsToShow: 2, itemsToScroll: 2 },
+      { width: 850, itemsToShow: 3 },
+      { width: 1150, itemsToShow: 4, itemsToScroll: 1 },
+    ];
+  }
   handleClick() {
     console.log("Click happened");
   }
@@ -24,31 +46,76 @@ class CheckOut extends Component {
                 title1="CHECKOUT FEATURES"
                 title2="The only app you will need"
               />
-              <ul>
-                <li>
-                  <i
-                    class="fas fa-long-arrow-alt-left fa-2x clscheme swiper-button-disabled"
-                    tabindex="0"
-                    role="button"
-                    aria-label="Next slide"
-                    aria-disabled="true"
-                  ></i>
-                </li>
-                <li>
-                  <i
-                    class="fas fa-long-arrow-alt-right fa-2x clscheme"
-                    tabindex="0"
-                    role="button"
-                    aria-label="Previous slide"
-                    aria-disabled="false"
-                    onClick={() => this.handleClick()}
-                  ></i>
-                </li>
-              </ul>
             </div>
             <div className="right">
               {" "}
-              <Swiper
+              <Carousel
+                renderArrow={this.myArrow}
+                itemPadding={[10, 18]}
+                breakPoints={this.breakPoints}
+                pagination={false}
+              >
+                <div className="slide">
+                  <img src="/images/img-icon1.png" alt="1" />
+                  <h3>Free Caliing Service</h3>
+                  <p>
+                    Direct mailing research development buyer iPad validation
+                    startup social proof learning curve user experience
+                    analytics
+                  </p>
+                </div>
+
+                <div className="slide">
+                  <img src="/images/img-icon2.png" alt="1" />
+                  <h3>Free Caliing Service</h3>
+                  <p>
+                    Direct mailing research development buyer iPad validation
+                    startup social proof learning curve user experience
+                    analytics
+                  </p>
+                </div>
+
+                <div className="slide">
+                  <img src="/images/img-icon3.png" alt="1" />
+                  <h3>Free Caliing Service</h3>
+                  <p>
+                    Direct mailing research development buyer iPad validation
+                    startup social proof learning curve user experience
+                    analytics
+                  </p>
+                </div>
+
+                <div className="slide">
+                  <img src="/images/img-icon1.png" alt="1" />
+                  <h3>Free Caliing Service</h3>
+                  <p>
+                    Direct mailing research development buyer iPad validation
+                    startup social proof learning curve user experience
+                    analytics
+                  </p>
+                </div>
+
+                <div className="slide">
+                  <img src="/images/img-icon2.png" alt="1" />
+                  <h3>Free Caliing Service</h3>
+                  <p>
+                    Direct mailing research development buyer iPad validation
+                    startup social proof learning curve user experience
+                    analytics
+                  </p>
+                </div>
+
+                <div className="slide">
+                  <img src="/images/img-icon3.png" alt="1" />
+                  <h3>Free Caliing Service</h3>
+                  <p>
+                    Direct mailing research development buyer iPad validation
+                    startup social proof learning curve user experience
+                    analytics
+                  </p>
+                </div>
+              </Carousel>
+              {/* <Swiper
                 // install Swiper modules
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
                 spaceBetween={10}
@@ -154,7 +221,7 @@ class CheckOut extends Component {
                     </p>
                   </div>
                 </SwiperSlide>
-              </Swiper>
+              </Swiper> */}
             </div>
           </div>
         </Container>
